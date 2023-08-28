d26fac0424b4c9567d2bfc4014bdd3506d3d22f5
Anders Soh <anders.soh@circles.asia>
2023-08-28 14:29:43 +0800

    init pref screen

---
+++ app/src/main/java/com/hakkasuru/arcana/debugprefs/PreferenceScreen.kt
@@ -0,0 +1,11 @@
+package com.hakkasuru.arcana.debugprefs
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PreferenceScreen() {
+    Surface {
+
+    }
+}
---
+++ app/src/main/java/com/hakkasuru/arcana/debugprefs/PreferenceViewModel.kt
@@ -0,0 +1,6 @@
+package com.hakkasuru.arcana.debugprefs
+
+import androidx.lifecycle.ViewModel
+
+class PreferenceViewModel : ViewModel() {
+}
